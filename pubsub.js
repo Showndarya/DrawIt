6b1d72ab84f19d433dcdc8d1e81abad4910f34b1
Ankit-22 <ankitmani.t@somaiya.edu>
2018-02-10 13:12:01 +0530

    Add comments

--- pubsub.js
+++ pubsub.js
@@ -1,13 +1,22 @@
+// This module serves as a mediator for all events
 var pubsub = {
 
+	// An event subscribes
 	on: (eventName, func) => {
+
+		// If events is not defined, define it
 		if(!this.events)
 			this.events = {};
+
+		//Add the function to event's list
 		this.events[eventName] = this.events[eventName] || [];
 		this.events[eventName].push(func);
 	},
 
+	// An event unsubscribes
 	off: (eventName, func) => {
+
+		//If there is an event loop in its list and find the function and remove it
 		if(this.events[eventName])
 			for( var index in this.events[eventName] )
 				if(this.events[eventName][index] === func) {
@@ -16,7 +25,10 @@ var pubsub = {
 				}
 	},
 
+	// An event is published
 	emit: (eventName, data) => {
+
+		// If event is present loop through it and execute the functions on its list with the data.
 		if(this.events[eventName])
 			this.events[eventName].forEach( func => {
 				func(data);
